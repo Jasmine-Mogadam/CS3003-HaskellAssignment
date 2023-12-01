ba1159a0f20dbf073ea1a707125470f57a44438b
Jasmine Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2023-11-30 21:54:14 -0500

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -3,23 +3,26 @@ module HaskellAssignment where
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
-data Found = Match Int | NoMatch deriving Eq
+data Found = Match Int | NoMatch deriving (Eq)
+
 instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
-  
+
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 findFirst func list
-    | null list = NoMatch -- if list is empty, return NoMatch
-    | func (head list) = Match 0 -- if head item matches, return Match 0
-    | otherwise = case findFirst func (tail list) of -- run again without head
-                    NoMatch -> NoMatch
-                    Match index -> Match (index + 1)  -- increase index of Match
+  | null list = NoMatch -- if list is empty, return NoMatch
+  | func (head list) = Match 0 -- if head item matches, return Match 0
+  | otherwise = case findFirst func (tail list) of -- run again without head
+      NoMatch -> NoMatch
+      Match index -> Match (index + 1) -- increase index of Match
 
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+palindrome [] = True -- empty strings are palindromes
+palindrome [x] = True -- one-character strings are palindromes
 palindrome c
-    | head c == last c = palindrome (init (tail c)) -- if the first and last are equal, continue with both removed
-    | otherwise = False -- if first and last are not equal, it is not a palindrome
+  | head c == last c = palindrome (init (tail c)) -- if the first and last are equal, continue with both removed
+  | otherwise = False -- if first and last are not equal, it is not a palindrome
